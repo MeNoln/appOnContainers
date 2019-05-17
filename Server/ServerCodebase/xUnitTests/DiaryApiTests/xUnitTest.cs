05dbecda7e1cee697bf3b69a5c4ec652a524297e
noln <mark3dom@mail.ru>
2019-05-18 01:42:21 +0300

    tests for both api created

--- Server/ServerCodebase/xUnitTests/DiaryApiTests/xUnitTest.cs
+++ Server/ServerCodebase/xUnitTests/DiaryApiTests/xUnitTest.cs
@@ -6,12 +6,17 @@ using DiaryApi.Models;
 using DiaryApi.Infrastructure;
 using System.Threading.Tasks;
 using DiaryApi.Controllers;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 
 namespace xUnitTests.DiaryApiTests
 {
     public class xUnitTest
     {
-        private IEnumerable<DiaryModel> GetTestDbData()
+        Mock<DiaryApi.Infrastructure.IRepo> mock = new Mock<DiaryApi.Infrastructure.IRepo>();
+
+        //Test Database Data
+        private List<DiaryModel> GetTestDbData()
         {
             return new List<DiaryModel>
             {
@@ -23,11 +28,109 @@ namespace xUnitTests.DiaryApiTests
         }
 
         [Fact]
-        public void Test()
+        public void DiaryController_GetAllMethod_Returns_AllData_From_Table()
         {
             //Arrange
+            
+            mock.Setup(repo => repo.GetAllDiaryNotes()).Returns(Task.FromResult(GetTestDbData() as IEnumerable<DiaryModel>));
+            var controller = new DiaryController(mock.Object);
             //Act
+            var result = controller.GetAll();
             //Assert
+            var viewResult = Assert.IsType<Task<IEnumerable<DiaryModel>>>(result);
+            var model = Assert.IsAssignableFrom<IEnumerable<DiaryModel>>(viewResult.Result);
+            Assert.Equal(GetTestDbData().Count, model.Count());
+            
+        }
+
+        [Fact]
+        public void DiaryController_GetCurrentMethod_Returns_JSON_Object()
+        {
+            //Arrange
+            DiaryModel testObject = new DiaryModel { Id = 1, Date = DateTime.Now, DayMark = "good day",
+                                                      DayDescription = "I wrote some xUnit tests!" };
+
+            mock.Setup(repo => repo.GetCurrentDiaryNote(testObject.Id)).Returns(Task.FromResult(testObject));
+            var controller = new DiaryController(mock.Object);
+            //Act
+            var result = controller.GetCurrent(testObject.Id);
+            //Assert
+            var viewResult = Assert.IsType<Task<IActionResult>>(result);
+            var model = Assert.IsAssignableFrom<JsonResult>(viewResult.Result);
+            Assert.Equal(testObject, model.Value);
+        }
+
+        [Fact]
+        public void DiaryController_GetCurrentMethod_Returns_NotFoundObject_Result()
+        {
+            //Arrange
+            int id = 1;
+
+            mock.Setup(repo => repo.GetCurrentDiaryNote(id)).Returns(Task.FromResult(null as DiaryModel));
+            var controller = new DiaryController(mock.Object);
+            //Act
+            var result = controller.GetCurrent(id);
+            //Assert
+            var view = Assert.IsType<Task<IActionResult>>(result);
+            Assert.IsAssignableFrom<NotFoundObjectResult>(view.Result);
+        }
+
+        [Fact]
+        public void DiaryController_CreateDiaryNoteMethod_Returns_OkObject_Result()
+        {
+            //Arrange
+            DiaryModel testObject = new DiaryModel { Id = 1, Date = DateTime.Now, DayMark = "good", DayDescription = "text" };
+
+            mock.Setup(repo => repo.Create(testObject));
+            var controller = new DiaryController(mock.Object);
+            //Act
+            var result = controller.CreateDiaryNote(testObject);
+            //Assert
+            Assert.IsType<OkObjectResult>(result);
+            mock.Verify(r => r.Create(testObject));
+        }
+
+        [Fact]
+        public void DiaryController_UpdateDiary_Returns_NoContent_Result()
+        {
+            //Arrange
+            int id = 1;
+            DiaryModel testObject = new DiaryModel { Id = id, Date = DateTime.Now, DayMark = "good", DayDescription = "text" };
+
+            var controller = new DiaryController(mock.Object);
+            //Act
+            var result = controller.UpdateDiary(id, testObject);
+            //Assert
+            Assert.IsType<NoContentResult>(result);
+            mock.Verify(r => r.Update(testObject));
+        }
+
+        [Fact]
+        public void DiaryController_UpdateDiary_Returns_BadRequest_Result()
+        {
+            //Arrange
+            int id = 1;
+            DiaryModel testObject = new DiaryModel { Id = 2, Date = DateTime.Now, DayMark = "good", DayDescription = "text" };
+
+            var controller = new DiaryController(mock.Object);
+            //Act
+            var result = controller.UpdateDiary(id, testObject);
+            //Assert
+            Assert.IsType<BadRequestResult>(result);
+        }
+
+        [Fact]
+        public void DiaryController_RemoveDiaryNote_Returns_NoContent_Result()
+        {
+            //Arrange
+            int id = 1;
+
+            var controller = new DiaryController(mock.Object);
+            //Act
+            var result = controller.RemoveDiaryNote(id);
+            //Assert
+            Assert.IsType<NoContentResult>(result);
+            mock.Verify(r => r.Delete(id));
         }
     }
 }
--- Server/ServerCodebase/xUnitTests/TodoListApiTests/xUnitTest.cs
+++ Server/ServerCodebase/xUnitTests/TodoListApiTests/xUnitTest.cs
@@ -1,11 +1,117 @@
 ﻿using System;
 using Xunit;
+using Moq;
+using DiaryApi.Infrastructure;
+using System.Collections.Generic;
+using TodoListApi.Models;
+using System.Threading.Tasks;
+using TodoListApi.Controllers;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 
 namespace xUnitTests.TodoListApiTests
 {
     public class xUnitTest
     {
+        Mock<TodoListApi.Infrastructure.IRepo> mock = new Mock<TodoListApi.Infrastructure.IRepo>();
+
+        private List<TodoModel> GetTestDbData()
+        {
+            return new List<TodoModel>
+            {
+                new TodoModel{ Id = 1, TaskName = "One", IsDone = false },
+                new TodoModel{ Id = 2, TaskName = "Two", IsDone = false },
+                new TodoModel{ Id = 3, TaskName = "Three", IsDone = false },
+                new TodoModel{ Id = 4, TaskName = "Four", IsDone = true },
+                new TodoModel{ Id = 5, TaskName = "Fice", IsDone = true },
+            };
+        }
+
         [Fact]
-        public void Test() { }
+        public void TodoConteroller_GetAllMethod_Returns_AllTodoNotes_Result()
+        {
+            //Arrange
+            mock.Setup(repo => repo.GetAllTodos()).Returns(Task.FromResult(GetTestDbData() as IEnumerable<TodoModel>));
+            var controller = new TodoController(mock.Object);
+            //Act
+            var result = controller.GetAll();
+            //Assert
+            var viewResult = Assert.IsType<Task<IEnumerable<TodoModel>>>(result);
+            var model = Assert.IsAssignableFrom<IEnumerable<TodoModel>>(viewResult.Result);
+            Assert.Equal(GetTestDbData().Count, model.Count());
+        }
+
+        [Fact]
+        public void TodoConteroller_GetAlreadyFinishedMethod_Returns_AllTodoNotes_Result()
+        {
+            //Arrange
+            mock.Setup(repo => repo.GetAlreadyFinishedTodos())
+                .Returns(Task.FromResult(GetTestDbData().Where(i => i.IsDone == true) as IEnumerable<TodoModel>));
+            var controller = new TodoController(mock.Object);
+            //Act
+            var result = controller.GetAllDone();
+            //Assert
+            var viewResult = Assert.IsType<Task<IEnumerable<TodoModel>>>(result);
+            var model = Assert.IsAssignableFrom<IEnumerable<TodoModel>>(viewResult.Result);
+            Assert.Equal(GetTestDbData().Where(i => i.IsDone == true).Count(), model.Count());
+        }
+
+        [Fact]
+        public void TodoController_CreateTodoNoteMethod_Returns_OkObject_Result()
+        {
+            //Arrange
+            TodoModel testObject = new TodoModel { Id = 1, TaskName = "good", IsDone = false};
+
+            mock.Setup(repo => repo.Create(testObject));
+            var controller = new TodoController(mock.Object);
+            //Act
+            var result = controller.CreateTodo(testObject);
+            //Assert
+            Assert.IsType<OkObjectResult>(result);
+            mock.Verify(r => r.Create(testObject));
+        }
+
+        [Fact]
+        public void TodoController_UpdateTodoNoteMethod_Returns_NoContent_Result()
+        {
+            //Arrange
+            int id = 1;
+            TodoModel testObject = new TodoModel { Id = 1, TaskName = "good", IsDone = false };
+
+            var controller = new TodoController(mock.Object);
+            //Act
+            var result = controller.UpdateTodo(id, testObject);
+            //Assert
+            Assert.IsType<NoContentResult>(result);
+            mock.Verify(r => r.Update(testObject));
+        }
+
+        [Fact]
+        public void TodoController_UpdateTodoNoteMethod_Returns_BadRequest_Result()
+        {
+            //Arrange
+            int id = 1;
+            TodoModel testObject = new TodoModel { Id = 2, TaskName = "good", IsDone = false };
+
+            var controller = new TodoController(mock.Object);
+            //Act
+            var result = controller.UpdateTodo(id, testObject);
+            //Assert
+            Assert.IsType<BadRequestResult>(result);
+        }
+
+        [Fact]
+        public void TodoController_RemoveTodo_Returns_NoContent_Result()
+        {
+            //Arrange
+            int id = 1;
+
+            var controller = new TodoController(mock.Object);
+            //Act
+            var result = controller.RemoveTodo(id);
+            //Assert
+            Assert.IsType<NoContentResult>(result);
+            mock.Verify(r => r.Delete(id));
+        }
     }
 }
